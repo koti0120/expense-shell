58bf70d7640384835200d4a6a23e9688da092dc3
KotiYadav <koti0120@gmail.com>
2024-12-17 14:53:29 +0530

    mysql configuration file

--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/etc/$SCRIPT_NAME-$TIMESTAMP.log
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
